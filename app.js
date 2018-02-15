31a1cb976e7967807ab9d2768ee7ebdad1eef26a
Jon Malek <jmalek@ns1.com>
2018-02-15 14:02:40 -0500

    Add all the stuff

--- app.js
+++ app.js
@@ -5,15 +5,51 @@ const controller = Botkit.slackbot({
     debug: false,
     require_delivery: true
 });
-const Scim = require("Scim");
-const scim = new Scim();
+const Scim = require("./scim");
+const scim = new Scim.Scim(config.oauth);
 
-controller.hears([".*"], ['direct_message'], function(bot, message) {
-    bot.reply(message, "Got it!");
-    bot.api.groups.list((err, res) => {
+controller.hears(["^user list$"], ['direct_message'], async (bot, message) => {
+    try {
+        let user_list = await scim.list_users();
+        let reply = "";
+        for (let user of user_list){
+            reply += await scim.get_username(user) + "\n";
+        }
+        bot.reply(message, reply);
+    }
+    catch (err) {
+        bot.reply(message, "Sorry! Something went wrong: " + err);
+    }
+});
+
+controller.hears(["^group list$"], ['direct_message'], async (bot, message) => {
+    try {
+        let group_list = await scim.list_groups();
+        for (let group in group_list){
+            let reply = await scim.get_groupname(group) + ":\n";
+            for (let user of group_list[group]){
+                reply += "\t" + await scim.get_username(user["value"]) + "\n";
+            }
+
+            bot.reply(message, reply);
+        }
+    }
+    catch (err) {
+        bot.reply(message, "Sorry! Something went wrong: " + err);
+    }
+});
+
+controller.hears(["^(add|remove) (.*) (to|from) (.*)$"], ['direct_message'], async (bot, message) => {
+    let action = message.match[1];
+    let user = message.match[2];
+    let group = message.match[4];
+    try {
+        await scim.group_change(user, group, action);
+    }
+    catch (err) {
         console.log(err);
-        console.log(res);
-    })
+    }
+    bot.reply(message, "Done! Type `group list` to view the new groups")
 });
 
 let bot = controller.spawn({
--- scim.js
+++ scim.js
@@ -1,19 +1,126 @@
 const request = require("request-promise-native");
-class Scim{
+
+function lookup_groupid(group_name, group_data){
+    let group_id = null;
+    for (let group of group_data["Resources"]){
+        if (group.displayName === group_name){
+            group_id = group.id;
+        }
+    }
+    return group_id;
+}
+
+function lookup_userid(user_name, user_data){
+    let user_id = null;
+    for (let user of user_data["Resources"]){
+        if (user.userName === user_name){
+            user_id = user.id;
+        }
+    }
+    return user_id;
+}
+
+class Scim {
     constructor(token){
-        this.options = {
-            'auth': {
-                'bearer': token
+        this.url = "https://api.slack.com/scim/v1/";
+        this.token = token;
+    }
+
+    async get_user_data(){
+        let options = {
+            url: this.url + "Users",
+            json: true,
+            headers: {
+                "Authorization": "Bearer " + this.token,
+                "Accept": "application/json"
             }
         };
+        return await request.get(options);
+    }
+
+    async get_username(userid){
+        let options = {
+            url: this.url + "Users/" + userid,
+            json: true,
+            headers: {
+                "Authorization": "Bearer " + this.token,
+                "Accept": "application/json"
+            }
+        };
+        let response = await request.get(options);
+        return response.userName;
+    }
+
+    async get_groupname(groupid){
+        let options = {
+            url: this.url + "Groups/" + groupid,
+            json: true,
+            headers: {
+                "Authorization": "Bearer " + this.token,
+                "Accept": "application/json"
+            }
+        };
+        let response = await request.get(options);
+        return response.displayName;
+    }
+
+    async get_group_data(){
+        let options = {
+            url: this.url + "Groups",
+            json: true,
+            headers: {
+                "Authorization": "Bearer " + this.token,
+                "Accept": "application/json"
+            }
+        };
+        return await request.get(options);
     }
 
     async list_users(){
-        let user_list = await request.get("https://api.slack.com/scim/v1/Users", this.options);
+        let response = await this.get_user_data();
+        let user_list = [];
+        for (let user of response["Resources"]){
+            user_list.push(user["id"])
+        }
         return user_list;
     }
 
+    async list_groups(){
+        let response = await this.get_group_data();
+        let group_list = {};
+        for (let group of response["Resources"]){
+            group_list[group["id"]] = group["members"];
+        }
+        return group_list;
+    }
 
+    async group_change(username, groupname, action){
+        let group_data = await this.get_group_data();
+        let group_id = lookup_groupid(groupname, group_data);
+        let user_data = await this.get_user_data();
+        let user_id = lookup_userid(username, user_data);
+        let options = {
+            url: this.url + "Groups/" + group_id,
+            json: true,
+            headers: {
+                "Authorization": "Bearer " + this.token,
+                "Accept": "application/json"
+            },
+            body: {
+                "schemas": ['urn:scim:schemas:core:1.0'],
+                "members": [
+                    {
+                        "value": user_id,
+                    }
+                ]
+            }
+        };
+        if (action === "remove"){
+            options["body"]["members"][0]["operation"] = "delete"
+        }
+        console.log(options);
+        let response = await request.patch(options);
+    }
 
 }
 
